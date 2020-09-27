512578f7ec649cea624c34c91c34b42268eaa1f2
Johan de Groot <johan.groot@webking.se>
2020-09-27 11:48:54 +0200

    ease test

---
+++ b_test/precache-manifest.4c050475e442b21a10f8f12acdd80f55.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90f57017e12801a655d4d42f7c8212c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15577b7a59faec16eccf",
+    "url": "/static/css/main.971e422f.chunk.css"
+  },
+  {
+    "revision": "2019e83f7c5e367124b2",
+    "url": "/static/js/2.26c8112e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.26c8112e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15577b7a59faec16eccf",
+    "url": "/static/js/main.ee151c37.chunk.js"
+  },
+  {
+    "revision": "662ab4cb5318750301d7",
+    "url": "/static/js/runtime-main.050e3184.js"
+  },
+  {
+    "revision": "86f75a08156098cbd0bf86f271345ebe",
+    "url": "/static/media/Background.86f75a08.png"
+  },
+  {
+    "revision": "9af59e1b98cf716f5ef335b0a0ea5bff",
+    "url": "/static/media/Moln_1.9af59e1b.png"
+  },
+  {
+    "revision": "1de98f6cab6d48c4c2171f0da67e8175",
+    "url": "/static/media/Moln_2.1de98f6c.png"
+  },
+  {
+    "revision": "1bfd0053c18055d1b0dcb23556e59d8d",
+    "url": "/static/media/Moln_3.1bfd0053.png"
+  },
+  {
+    "revision": "a886f1d9197892298985af21e175eb13",
+    "url": "/static/media/Moln_4.a886f1d9.png"
+  },
+  {
+    "revision": "d085720ebe009855d36c241c127b621e",
+    "url": "/static/media/camera.d085720e.jpg"
+  },
+  {
+    "revision": "7932f094e2b9bbbbc11751ab76f997ae",
+    "url": "/static/media/lightCloud1.7932f094.png"
+  },
+  {
+    "revision": "b8a19b848f95ed964844e1a024515b61",
+    "url": "/static/media/lightCloud2.b8a19b84.png"
+  },
+  {
+    "revision": "a99358c2538edd21d58ab78879469b1c",
+    "url": "/static/media/lightCloud3.a99358c2.png"
+  },
+  {
+    "revision": "e1127783b551d9a81d6c7a74f528ce73",
+    "url": "/static/media/lightCloud4.e1127783.png"
+  },
+  {
+    "revision": "83c31cab2dcf3846dd6ed3c3cd0c4d67",
+    "url": "/static/media/lightSky.83c31cab.png"
+  },
+  {
+    "revision": "ea9ea516851bcc936e792cbe23ba3aa3",
+    "url": "/static/media/logo.ea9ea516.png"
+  },
+  {
+    "revision": "396b18be7bae6090df2583694bac07b8",
+    "url": "/static/media/logoDark.396b18be.png"
+  },
+  {
+    "revision": "a1118995a874e989437e4f13f19eece9",
+    "url": "/static/media/logowhite.a1118995.png"
+  }
+]);
